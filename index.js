c1423acb3e103897dd624990d2aece947f24f2d7
iAmAnkurit <bankurit1891@gmail.com>
2024-10-25 20:01:09 +0530

    Added dynamic routing

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hi");
+  res.render("index");
+});
+
+app.get("/profile/:username", (req, res) => {
+  const username = req.params.username;
+  res.end(`${username}'s profile`);
+});
+
+app.get("/about/:username/:age", (req, res) => {
+  const username = req.params.username;
+  const age = req.params.age;
+
+  res.end(`Welcome ${username} with age ${age}`);
 });
 
 app.listen(3000, () => {
